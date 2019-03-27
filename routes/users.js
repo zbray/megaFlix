a5bdf43cdb5c22670b90a2ee1616ef6302794795
Chad Dunnam <chad.dunnam@gmail.com>
2019-03-27 07:14:07 -0500

    postrequesttoregistrationform

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,10 @@ router.get('/login', (req, res) => res.render('login'));
 // Register page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register handle...so when we submit registration form make a post request to /users/register
+router.post('/register', (req, res) => {
+    console.log(req.body)
+    res.send('hello');
+});
 
 module.exports = router;
