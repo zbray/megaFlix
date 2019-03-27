6a0b4036127513db50a07611e8c3abbcb1c1b720
Chad Dunnam <chad.dunnam@gmail.com>
2019-03-27 07:34:06 -0500

    addedvalidationtoroutes

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,36 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register handle...so when we submit registration form make a post request to /users/register
 router.post('/register', (req, res) => {
-    console.log(req.body)
-    res.send('hello');
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //check required fields
+    if(!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please complete all fields'});
+    }
+
+    //check for matching passwords
+    if(password !== password2) {
+        errors.push({ msg: 'Passwords do not match'})
+    }
+
+    //confirm password length => 6 char
+    if(password.length <6) {
+        errors.push({ msg: 'Password should be at least 6 characters'})
+    }
+
+    if(errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+
+    } else {
+        res.send('pass');
+    }
 });
 
 module.exports = router;
