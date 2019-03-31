bb0bc18155ce9b85af25ff944b6f87c1cfdb7ab0
Ryan Gartner <ryanjgartner@gmail.com>
2019-03-31 07:53:44 -0500

    passport edit

--- models/user.js
+++ models/user.js
@@ -17,12 +17,12 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    // If manager redirect to manager page
-    isManager: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
     }
+    // If manager redirect to manager page
+    // isManager: {
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false
+    // }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
