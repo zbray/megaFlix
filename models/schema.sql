0b14e6d5f95e6d07607ac7b74ce05d627c1bde9b
shehanpeiris <shehanpeiris@gmail.com>
2019-03-31 16:45:54 -0500

    fixed...maybe

--- models/schema.sql
+++ models/schema.sql
@@ -10,9 +10,18 @@ CREATE TABLE films (
   price INTEGER NOT NULL,
   format VARCHAR (50) NULL,
   isReserved BOOLEAN NOT NULL DEFAULT 0,
-
   PRIMARY KEY (id)
 );
+
+CREATE TABLE Users (
+ id INTEGER NOT NULL auto_increment,
+ email VARCHAR(255) NOT NULL UNIQUE,
+ password VARCHAR(255) NOT NULL,
+ createdAt DATETIME NOT NULL,
+ updatedAt DATETIME NOT NULL,
+ PRIMARY KEY (id)
+);
+
 
 
 SELECT * FROM films;
