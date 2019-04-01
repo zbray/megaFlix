24006821815c338189452953042680dbc7cf5007
shehanpeiris <shehanpeiris@gmail.com>
2019-04-01 13:30:44 -0500

    manager page pulling from db, still no CRUD functionality

--- public/js/manager.js
+++ public/js/manager.js
@@ -2,6 +2,57 @@ $(document).ready(function() {
   // Activate tooltips
   // $("[data-toggle="tooltip"]").tooltip();
 
+  var dbTable = $("#db-table"); 
+  // Function to add rows to table
+  // function addRow() {
+  //   var movieID 
+  // };
+  
+  dbPull();
+
+  // Function to pull movies from db
+  function dbPull() {
+    $.get("/api/movies/", function(dbData) {
+      return dbData;
+    }).then(function(response) {
+      var dbMovies = response;
+      console.log("Testing db pull", dbMovies);
+      for (let k = 0; k < dbMovies.length; k++) {
+        var movieID = dbMovies[k].id;
+        var movieTitle = dbMovies[k].title;
+        var movieYear = dbMovies[k].year;
+        var movieGenre = dbMovies[k].genre;
+        var moviePrice = Math.trunc(dbMovies[k].price);
+        var movieFormat = dbMovies[k].format;
+        var movieReserved = dbMovies[k].isReserved;
+        if (movieReserved) {
+          var reserved = "<td><span class='custom-checkbox'><input type='checkbox' id='reserved' name='options[]' value='1' checked><label for='reserved'></label></span></td>";
+          var checkIn = "<td><span class='custom-checkbox'><input type='checkbox' id='checkIn' name='options[]' value='1'><label for='checkIn'></label></span></td>";
+          var checkOut = "<td><span class='custom-checkbox'><input type='checkbox' id='checkOut' name='options[]' value='1'><label for='checkOut'></label></span></td>";
+          var reservedCols = reserved + checkIn + checkOut;
+        } else {
+          var reserved = "<td><span class='custom-checkbox'><input type='checkbox' id='reserved' name='options[]' value='1'><label for='reserved'></label></span></td>";
+          var checkIn = "<td><span class='custom-checkbox'><input type='checkbox' id='checkIn' name='options[]' value='1'><label for='checkIn'></label></span></td>";
+          var checkOut = "<td><span class='custom-checkbox'><input type='checkbox' id='checkOut' name='options[]' value='1'><label for='checkOut'></label></span></td>";
+          var reservedCols = reserved + checkIn + checkOut;
+        }
+
+        // Create html for new table rows
+        var idCol = "<td>" + movieID + "</td>";
+        var titleCol = "<td>" + movieTitle + "</td>";
+        var yearCol = "<td>" + movieYear + "</td>";
+        var genreCol = "<td>" + movieGenre + "</td>";
+        var priceCol = "<td>" + moviePrice + "</td>";
+        var formatCol = "<td>" + movieFormat + "</td>";
+        var modalCols = "<td><a href='#editMovieModal' class='edit' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='edit'>&#xE254;</i></a><a href='#deleteMovieModal' class='delete' data-toggle='modal'><i class='material-icons' data-toggle='tooltip' title='delete'>&#xE872;</i></a></td>";
+        var rowData = "<tr>" + idCol + titleCol + yearCol + genreCol + priceCol + formatCol + reservedCols + modalCols + "</tr>";
+        
+        // Append new row (comprised of data from db) to table
+        dbTable.append(rowData);
+      }
+    });
+  }
+
   // Filter table rows based on searched term
   $("#search").on("keyup", function() {
     var term = $(this)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,12 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 module.exports = function(app) {
+  // POST route to send user login credentials to DB for authentication
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.redirect("/customer-home");
   });
 
+  // POST route to send new user signup credentials to DB for future authentication.
   app.post("/api/signup", function(req, res) {
     console.log(req.body);
     db.User.create({
